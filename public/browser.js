89c326afef7bc284352be67d65eabb6183a67beb
illi0125 <illikamalov10@gmail.com>
2026-04-28 09:15:21 +0900

    Last step logic of Reja project

--- public/browser.js
+++ public/browser.js
@@ -47,4 +47,36 @@ document.addEventListener("click", function (e) {
         });
     }
   }
+
+  // edit oper
+  if (e.target.classList.contains("edit-me")) {
+    let userInput = prompt(
+      "Edit something",
+      e.target.parentElement.parentElement.querySelector(".item-text")
+        .innerHTML,
+    );
+    if (userInput) {
+      axios
+        .post("/edit-item", {
+          id: e.target.getAttribute("data-id"),
+          new_input: userInput,
+        })
+        .then((response) => {
+          console.log(response.data);
+          e.target.parentElement.parentElement.querySelector(
+            ".item-text",
+          ).innerHTML = userInput;
+        })
+        .catch((err) => {
+          console.log("Please, try again!");
+        });
+    }
+  }
+});
+
+document.getElementById("clean-al").addEventListener("click", function () {
+  axios.post("/delete-all", { delete_all: true }).then((response) => {
+    alert(response.data.state);
+    document.location.reload();
+  });
 });
